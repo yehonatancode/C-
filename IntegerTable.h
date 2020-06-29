2c2fd3a9f172e3f662ec55c191d0b0b44f94c332
CodeJohnathan <33619145+CodeJohnathan@users.noreply.github.com>
2020-06-29 17:10:21 +0300

    finished abstract, static, virtual methodology

--- IntegerTable.h
+++ IntegerTable.h
@@ -1,5 +1,18 @@
 #pragma once
 
+#include <iostream>
+#include 
+
 class IntegerTable: public AbstractTable{
-    
-}
+    int* values;
+    void print(){
+        cout << "Integer table with values" << endl;
+        for(int i=0; i<size; i++) cout << values[i] + " ";
+    }
+    IntegerTable(int size): AbstractTable(size), values(new int[size]){
+        for(int i=0;i<size;++i) values[i] = "%";
+    }
+    ~IntegerTable(){
+        delete [] values;
+    }
+};
